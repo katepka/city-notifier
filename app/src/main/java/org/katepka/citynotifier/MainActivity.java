bac4db37eb41fb4ceec9cb2e56eba33a73c553a2
katepka <katepka92@gmail.com>
2020-05-18 22:41:54 +0300

    Added MainActivity skeleton. Added MenuActivity.

--- app/src/main/java/org/katepka/citynotifier/MainActivity.java
+++ app/src/main/java/org/katepka/citynotifier/MainActivity.java
@@ -2,13 +2,30 @@ package org.katepka.citynotifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Opens menu activity
+     * @param view
+     */
+    public void onClickOpenMenu(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickCreateMessage(View view) {
+    }
+
+    public void onClickCreateIdea(View view) {
     }
 }
