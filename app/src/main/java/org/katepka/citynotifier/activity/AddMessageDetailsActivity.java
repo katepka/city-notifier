3641f464624f26c1c8cf1679d3d655c5fb2e21eb
katepka <katepka92@gmail.com>
2020-05-25 22:31:33 +0300

    Added onItemClickListener for category list

--- app/src/main/java/org/katepka/citynotifier/activity/AddMessageDetailsActivity.java
+++ app/src/main/java/org/katepka/citynotifier/activity/AddMessageDetailsActivity.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import org.katepka.citynotifier.R;
 
 public class AddMessageDetailsActivity extends AppCompatActivity {
 
+    private Button buttonChooseCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_message_details);
+
+        buttonChooseCategory = findViewById(R.id.buttonChooseCategory);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("category")) {
+            String category = intent.getStringExtra("category");
+            buttonChooseCategory.setText(category);
+        }
     }
 
     public void onClickBackToAddress(View view) {
--- app/src/main/java/org/katepka/citynotifier/activity/CategoryListActivity.java
+++ app/src/main/java/org/katepka/citynotifier/activity/CategoryListActivity.java
@@ -2,18 +2,36 @@ package org.katepka.citynotifier.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import org.katepka.citynotifier.R;
 
 public class CategoryListActivity extends AppCompatActivity {
 
+    private ListView listViewCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_list);
+
+        listViewCategory = findViewById(R.id.listViewCategory);
+        int items = listViewCategory.getCount();
+
+        listViewCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("ShowToast")
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), AddMessageDetailsActivity.class);
+                intent.putExtra("category", listViewCategory.getItemAtPosition(position).toString());
+                startActivity(intent);
+            }
+        });
     }
 
     public void onClickBackToMessageDetails(View view) {
